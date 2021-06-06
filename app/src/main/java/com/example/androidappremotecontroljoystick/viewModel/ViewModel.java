001a302049a631d144d004b10781a4d1396d987d
noamb <noambeer4@gmail.com>
2021-06-06 13:50:16 +0300

    initial file creation

---
+++ app/src/main/java/com/example/androidappremotecontroljoystick/viewModel/ViewModel.java
@@ -0,0 +1,4 @@
+package com.example.androidappremotecontroljoystick.viewModel;
+
+public class ViewModel {
+}
---
+++ app/src/main/java/com/example/androidappremotecontroljoystick/views/Joystick.java
@@ -0,0 +1,11 @@
+package com.example.androidappremotecontroljoystick.views;
+
+import android.content.Context;
+import android.view.View;
+
+public class Joystick extends View {
+//    might need to change
+    public Joystick(Context context) {
+        super(context);
+    }
+}
