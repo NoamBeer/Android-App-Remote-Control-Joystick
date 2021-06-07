be1f864900988f93cddb563adc93507ae4426838
noamb <noambeer4@gmail.com>
2021-06-07 17:07:49 +0300

    added documentation

--- app/src/main/java/com/example/androidappremotecontroljoystick/model/FGModel.java
+++ app/src/main/java/com/example/androidappremotecontroljoystick/model/FGModel.java
@@ -13,22 +13,48 @@ public class FGModel {
     BlockingQueue<Runnable> dispatchQueue = new LinkedBlockingQueue<>();
     boolean stop = false;
 
+    /**
+     * Sets the aileron value in FG.
+     *
+     * @param aileron value to set.
+     */
     public void setAileron(double aileron) {
         send("aileron ", String.valueOf(aileron));
     }
 
+    /**
+     * Sets the elevator value in FG.
+     *
+     * @param elevator value to set.
+     */
     public void setElevator(double elevator) {
         send("elevator ", String.valueOf(elevator));
     }
 
+    /**
+     * Sets the rudder value in FG.
+     *
+     * @param rudder value to set.
+     */
     public void setRudder(double rudder) {
         send("rudder ", String.valueOf(rudder));
     }
 
+    /**
+     * Sets the throttle value in FG.
+     *
+     * @param throttle value to set.
+     */
     public void setThrottle(double throttle) {
         send("current-engine/throttle ", String.valueOf(throttle));
     }
 
+    /**
+     * Connects to FG.
+     *
+     * @param host - ip address for the socket.
+     * @param port - port for the socket.
+     */
     public void connect(String host, int port) {
         try {
             fgConnection = new Socket(host, port);
@@ -38,11 +64,17 @@ public class FGModel {
         }
     }
 
+    /**
+     * Closes the communication with FG.
+     */
     public void disconnect() {
         stop = true;
         t.interrupt();
     }
 
+    /**
+     * Runs commands from dispatchQueue sequentially.
+     */
     public void start() {
         t = new Thread(() -> {
             while (!stop) {
@@ -56,6 +88,11 @@ public class FGModel {
         t.start();
     }
 
+    /**
+     * Sends a command to FG by puting it in dispatchQueue.
+     * @param parameter to set.
+     * @param value to set.
+     */
     public void send(String parameter, String value) {
         try {
             dispatchQueue.put(() -> {
