f50ed4b20085357bfa2568ca5411bf1cb5e4e12a
noamb <noambeer4@gmail.com>
2021-06-07 15:40:02 +0300

    created MVVM foundations

--- app/src/main/java/com/example/androidappremotecontroljoystick/model/FGModel.java
+++ app/src/main/java/com/example/androidappremotecontroljoystick/model/FGModel.java
@@ -1,4 +1,44 @@
 package com.example.androidappremotecontroljoystick.model;
 
 public class FGModel {
+    private double aileron;
+    private double elevator;
+    private double rudder;
+    private double throttle;
+
+    public void setAileron(double aileron) {
+        this.aileron = aileron;
+        //TODO: send to FG
+    }
+
+    public void setElevator(double elevator) {
+        this.elevator = elevator;
+        //TODO: send to FG
+    }
+
+    public void setRudder(double rudder) {
+        this.rudder = rudder;
+        //TODO: send to FG
+    }
+
+    public void setThrottle(double throttle) {
+        this.throttle = throttle;
+        //TODO: send to FG
+    }
+
+    //    TODO
+    public void connect(String host, int port) {
+    }
+
+    //    TODO
+    public void disconnect() {
+    }
+
+    //    TODO
+    public void start() {
+    }
+
+    //    TODO
+    public void send(String command) {
+    }
 }
--- app/src/main/java/com/example/androidappremotecontroljoystick/viewModel/ViewModel.java
+++ app/src/main/java/com/example/androidappremotecontroljoystick/viewModel/ViewModel.java
@@ -1,4 +1,37 @@
 package com.example.androidappremotecontroljoystick.viewModel;
 
-public class ViewModel {
+import com.example.androidappremotecontroljoystick.model.FGModel;
+
+public class ViewModel extends androidx.lifecycle.ViewModel {
+    private FGModel fgModel;
+    private double VMaileron;
+    private double VMelevator;
+    private double VMrudder;
+    private double VMthrottle;
+
+
+    public ViewModel(FGModel fgModel) {
+        this.fgModel = fgModel;
+    }
+
+    public void setVMaileron(double VMaileron) {
+        fgModel.setAileron(VMaileron);
+    }
+
+    public void setVMelevator(double VMelevator) {
+        fgModel.setElevator(VMelevator);
+    }
+
+    public void setVMrudder(double VMrudder) {
+        fgModel.setRudder(VMrudder);
+    }
+
+    public void setVMthrottle(double VMthrottle) {
+        fgModel.setThrottle(VMthrottle);
+    }
+
+    //    TODO
+    public void connect(String host, int port) {
+        fgModel.connect(host, port);
+    }
 }
